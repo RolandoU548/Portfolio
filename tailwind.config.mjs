78600a4194e1bced8d6f364bdf7e6ffe39a68a88
RolandoU548 <rolandou548@gmail.com>
2025-03-25 19:39:36 -0400

    sin la barra central

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "Roboto",
+          '"Helvetica Neue"',
+          '"Arial Nova"',
+          '"Nimbus Sans"',
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
