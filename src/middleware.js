fa3b3fa3c89ed074b0548f86ae74e27898e8d51b
Rolando <rolandou548@gmail.com>
2025-11-30 03:47:18 -0400

    exchanged vercel.json and middleware

--- src/middleware.js
+++
@@ -1,8 +0,0 @@
-export function onRequest({ url, request, redirect }) {
-  if (url.pathname === "/") {
-    const langHeader = request.headers.get("accept-language");
-    const lang = langHeader?.split(",")[0].split("-")[0] || "es";
-    return redirect(lang === "en" ? "/en" : "/es", 302);
-  }
-  return; // 👈 importante
-}
