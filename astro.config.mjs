22d328105aca431e369a516b07043adf9a9fc88b
Rolando <rolandou548@gmail.com>
2025-11-30 02:59:00 -0400

    ¨outputserver¨

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
