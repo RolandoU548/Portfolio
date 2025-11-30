eabe83a40b5723c75ee1057623e703402ec509e2
Rolando <rolandou548@gmail.com>
2025-11-30 03:27:27 -0400

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/server";
+
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
